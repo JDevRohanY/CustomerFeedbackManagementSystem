d744ccd638a3e7f79762dcd9340fd4cf1e1c3702
Rohan Yadav <rohanyadav.career@gmail.com>
2026-04-30 13:37:15 +0530

    Added generic exception handler function

--- src/main/java/com/JDevRohanY/customerFeedbackManagementSystem/exceptions/GlobalExceptions.java
+++ src/main/java/com/JDevRohanY/customerFeedbackManagementSystem/exceptions/GlobalExceptions.java
@@ -1,11 +1,13 @@
 package com.JDevRohanY.customerFeedbackManagementSystem.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptions {
 
     @ExceptionHandler
@@ -21,5 +23,11 @@ public class GlobalExceptions {
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleFeedbackNotFoundException(FeedbackNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage(), 404));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleGenericExceptions(Exception ex){
+        log.error("Some exception occurred, exception : " +  ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getMessage(), 500));
     }
 }
