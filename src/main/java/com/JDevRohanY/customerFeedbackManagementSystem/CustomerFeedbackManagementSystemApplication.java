8530482b05dbf42c502a838c23dcf44a742d045a
Rohan Yadav <rohanyadav.career@gmail.com>
2026-04-30 12:06:17 +0530

    Initial commit

---
+++ src/main/java/com/JDevRohanY/customerFeedbackManagementSystem/CustomerFeedbackManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.JDevRohanY.customerFeedbackManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFeedbackManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerFeedbackManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JDevRohanY/customerFeedbackManagementSystem/CustomerFeedbackManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JDevRohanY.customerFeedbackManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFeedbackManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
